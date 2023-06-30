07c54dc11841626b62e015bc29d4ac2c07c2df7e
SimonW1223 <133788429+SimonW1223@users.noreply.github.com>
2023-06-30 19:41:23 +0200

    A

--- api/main.py
+++ api/main.py
@@ -1,43 +1,18 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+import customers
 
 app = FastAPI()
 
-# Przykładowa baza danych klientów
-customers = [
-    {
-        'id': 1,
-        'name': 'John Doe',
-        'email': 'john@example.com'
-    },
-    {
-        'id': 2,
-        'name': 'Jane Smith',
-        'email': 'jane@example.com'
-    }
-]
+origins = ["*"]
 
-# Model danych klienta
-class Customer(BaseModel):
-    id: int
-    name: str
-    email: str
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Endpoint do dodawania klientów
-@app.get('/customers')
-def add_customer(customer: Customer):
-    customers.append(customer.dict())
-    return {'message': 'Klient dodany pomyślnie.'}
-
-# Endpoint do edytowania klientów
-@app.put('/customers/{customer_id}')
-def edit_customer(customer_id: int, customer: Customer):
-    for idx, cust in enumerate(customers):
-        if cust['id'] == customer_id:
-            customers[idx] = customer.dict()
-            return {'message': 'Klient zaktualizowany pomyślnie.'}
-    return {'message': 'Nie znaleziono klienta o podanym ID.'}
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+app.include_router(customers.router, prefix="/customers")
